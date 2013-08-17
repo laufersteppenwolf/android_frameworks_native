58cea69808f2fee96c47ba8f50a7369f2dd51584
Tom <y>
2013-08-17 09:44:58 +0100

    Added missing MR0_CAMERA_BLOB ifdef

--- include/gui/SurfaceComposerClient.h
+++ include/gui/SurfaceComposerClient.h
@@ -74,7 +74,7 @@ public:
     /* triggers screen on and waits for it to complete */
     static void unblankDisplay(const sp<IBinder>& display);
 
-#if defined(ICS_CAMERA_BLOB)
+#if defined(ICS_CAMERA_BLOB) || defined(MR0_CAMERA_BLOB)
     static ssize_t getDisplayWidth(int32_t displayId);
     static ssize_t getDisplayHeight(int32_t displayId);
     static ssize_t getDisplayOrientation(int32_t displayId);
